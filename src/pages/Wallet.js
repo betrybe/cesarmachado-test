f2a750c745eee6937131d48189bf67400bc01b75
César Machado <cesarmach19@gmail.com>
2021-11-14 17:27:51 -0300

    Criando o formulário o header e a table para mostrar as informações, coloquei o css e fiz a lógica para usar o estado do redux

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,8 +1,23 @@
 import React from 'react';
+// import { connect } from 'react-redux';
+// import PropTypes from 'prop-types';
+import Form from '../components/form';
+import Header from '../components/header';
+import Table from '../components/table';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    return (
+      <div>
+        <div>
+          <Header />
+          <main>
+            <Form />
+            <Table />
+          </main>
+        </div>
+      </div>
+    );
   }
 }
 
